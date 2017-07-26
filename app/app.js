37fe954fa0c8135558e98f1cc4c0dea389a10572
Aubrian Halili <aubrian@istackmanila.com>
2017-07-26 15:14:52 +0800

    Added React Router

--- app/app.js
+++ app/app.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Component from './components/Component/Component';
 
-ReactDOM.render(
-  <Component />,
-  document.getElementById('root')
-);
+import { Router } from 'react-router';
+import { Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+
+ReactDOM.render((
+  <Router history={history}>
+    <Route component={Component} />
+  </Router>
+), document.getElementById('root'));
