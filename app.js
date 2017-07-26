cb85597915097a0530e9724e319000c2c104bb9f
Aubrian Halili <aubrian@istackmanila.com>
2017-07-26 13:46:56 +0800

    Added React and Webpack

--- app.js
+++ app.js
@@ -9,21 +9,57 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'ejs');
+
+
+
+
+const webpackConfig = require('./webpack.config');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const compiler = webpack(webpackConfig);
+const middleware = webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath,
+  silent: true,
+  stats: 'errors-only',
+});
+
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+
+// Since webpackDevMiddleware uses memory-fs internally to store build
+// artifacts, we use it instead
+const fs = middleware.fileSystem;
+
+app.get('*', (req, res) => {
+  fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(file.toString());
+    }
+  });
+});
+
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +76,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
   console.log('500 <----------- here');
 });
 
